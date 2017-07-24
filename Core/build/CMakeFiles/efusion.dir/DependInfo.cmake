201c15ca30b07eb3b6cc20e0381fec18e859f51a
Ric92 <ricloplop@gmail.com>
2017-07-24 19:17:04 +0200

    Nuevo repositorio

---
+++ Core/build/CMakeFiles/efusion.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Cuda/containers/device_memory.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Cuda/containers/device_memory.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Deformation.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Deformation.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/ElasticFusion.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/ElasticFusion.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Ferns.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Ferns.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/GPUTexture.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/GPUTexture.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/GlobalModel.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/GlobalModel.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/IndexMap.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/IndexMap.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Shaders/ComputePack.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Shaders/ComputePack.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Shaders/FeedbackBuffer.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Shaders/FeedbackBuffer.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Shaders/FillIn.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Shaders/FillIn.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Shaders/Resize.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Shaders/Resize.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Shaders/Vertex.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Shaders/Vertex.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/CholeskyDecomp.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/CholeskyDecomp.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/DeformationGraph.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/DeformationGraph.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/Intrinsics.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/Intrinsics.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/Parse.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/Parse.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/RGBDOdometry.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/RGBDOdometry.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/Resolution.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/Resolution.cpp.o"
+  "/home/rik/Descargas/elastic/ElasticFusion/Core/src/Utils/WindowsExtras.cpp" "/home/rik/Descargas/elastic/ElasticFusion/Core/build/CMakeFiles/efusion.dir/Utils/WindowsExtras.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/rik/ElasticFusion/deps/Pangolin/include"
+  "/home/rik/ElasticFusion/deps/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/usr/local/cuda/include"
+  "/usr/include/suitesparse"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ README.md
@@ -0,0 +1,2 @@
+# Elastic
+OpenNi2 y Pangolin tienen que estar disponibles en la carpeta anterior. ../OpenNi2 ../Pangolin
